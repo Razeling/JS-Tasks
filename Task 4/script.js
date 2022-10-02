4c67f94d3b44d9e17be2d71411c4013137b01be1
Razeling <n.apulskis@gmail.com>
2022-10-02 22:34:15 +0300

    Task 4 without CSS done

--- Task 4/script.js
+++ Task 4/script.js
@@ -10,3 +10,18 @@ turėti bent minimalų stilių ir būti responsive;
 -------------------------------------------------------------------------- */
 
 const ENDPOINT = 'cars.json';
+
+function printCarBrands() {
+    output = document.getElementById('output')
+    fetch(ENDPOINT)
+    .then((response) => response.json())
+    .then((data) =>{
+        for (let i = 0; i < data.length; i++) {
+            let newDiv = document.createElement("div");
+            newDiv.className = "Card";
+            let newContent;
+            newDiv.innerHTML = 'Brand: ' + data[i].brand + '<br>' + 'Models:  ' + data[i].models + '<br><br>'
+            output.appendChild(newDiv)
+        }
+    }
+)};
