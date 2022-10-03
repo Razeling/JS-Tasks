1241fc48d71b7cdb8edf691035900f36cea0cd81
Razeling <n.apulskis@gmail.com>
2022-10-03 19:04:33 +0300

    Task 9 done

--- Task 9/script.js
+++ Task 9/script.js
@@ -9,3 +9,35 @@ budget: number
 Metodas: 
 wasExpensive() - jeigu filmo "budget" yra daugiau nei 100 000 000 mln USD, tada grąžins true, kitu atveju false. 
 ------------------------------------------------------------------------------------------------------ */
+
+class Movie {
+    constructor(title, director, budget){
+        this.title = title;
+        this.director = director;
+        this.budget = budget;
+    }
+    wasExpensive() {
+        if (this.budget >= 100000000) {
+            return `Was the movie expensive? - true` 
+        }
+        else return `Was the movie expensive? - false` 
+    }
+}
+
+const movie1 = new Movie (
+    'Shrek 5',
+    'Rob Schneider',
+    '500000'
+)
+
+const movie2 = new Movie (
+    'Cats 2',
+    'Steven Spielberg',
+    '150000000'
+)
+
+console.log(movie1);
+console.log(movie1.wasExpensive());
+
+console.log(movie2);
+console.log(movie2.wasExpensive());
